03279fb21b4cd95a77611f3dd3001ff504b7e188
Daniel <dani@example.com>
2022-02-10 17:57:23 +0100

    Cambio en el fichero install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # install stuff we need
-apt-get update -y
-apt-get install -y python3 python3-pip python3-openssl gcc-mingw-w64 nasm upx binutils wget apt-transport-https procps mono-complete less metasploit-framework
+apt update -y
+apt install -y python3 python3-pip python3-openssl gcc-mingw-w64 nasm upx binutils wget apt-transport-https procps mono-complete less metasploit-framework
 python3 -m pip install -r /root/shad0w/requirements.txt
 
 # install dotnet
